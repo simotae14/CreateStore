baeb02106fad4c3cad2997b2f466154c269f1fea
Simone Taeggi <simone.taeggi@leovegas.com>
2018-06-28 11:38:56 +0200

    Managing more State Actions

--- index.js
+++ index.js
@@ -45,12 +45,20 @@ function createStore (reducer) {
 App Code
 */
 function todos (state = [], action) {
-    // controllo il tipo di azione occorsa
-    if (action.type === 'ADD_TODO') {
-        return state.concat([action.todo]);
+    // creo uno switch
+    switch(action.type) {
+        case 'ADD_TODO' :
+            return state.concat([action.todo]);
+        case 'REMOVE_TODO' :
+            // rimuovo il todo passato
+            return state.filter((todo) => todo.id !== action.id);
+        case 'TOGGLE_TODO' :
+            // modifico il valore del complete per il dato todo
+            return state.map((todo) => todo.id !== action.id ? todo :
+                Object.assign({}, todo, { complete: !todo.complete }));
+        default:
+            return state;
     }
-
-    return state;
 }
 
 
